7d26b1c8df3e7aece17d8b59dcdf477439f86191
Yongsun Lee <katarite715@naver.com>
2018-03-12 12:10:44 +0900

    Lecture 02
    Lecture 02
    삼각형 그리기

---
+++ 실습/Lecture 02/GLSLBase/Renderer.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+#include "Dependencies\glew.h"
+
+class Renderer
+{
+public:
+	Renderer(int windowSizeX, int windowSizeY);
+	~Renderer();
+
+	bool IsInitialized();
+	void Test();
+
+	// Lecture02 ½Ç½À
+	void Lecture2();
+
+private:
+	void Initialize(int windowSizeX, int windowSizeY);
+	bool ReadFile(char* filename, std::string *target);
+	void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType);
+	GLuint CompileShaders(char* filenameVS, char* filenameFS);
+	void CreateVertexBufferObjects();
+
+	bool m_Initialized = false;
+	
+	unsigned int m_WindowSizeX = 0;
+	unsigned int m_WindowSizeY = 0;
+
+	GLuint m_VBORect = 0;
+	GLuint m_SolidRectShader = 0;
+
+	GLuint m_Lecture2 = 0;
+
+};
+
